19d219b3c0bad589adf7a630e70d321a492ffde4
Yoav Matchulsky <yoavmatchulsky@gmail.com>
2012-03-31 14:46:56 +0300

    Added SegoeUI font (ttf, eot) - wasn't checked in Chrome/Opera/IE Added images to homepage Fixed some layout and styling problems Fixed links to use home_url() and servula_info()

--- home.php
+++ home.php
@@ -6,26 +6,33 @@
   <h2>All services combined in one easy-to-use platform: Content Writing, Link Building and more.</h2>
 
   <div id="leftcolumn">
-    <p>Move your mouse on each floor to see how it works</p>
-    <a href="http://test.servula.com/">
-      <img src="<?php bloginfo('template_url'); ?>/images/start-shopping.png" />
-    </a>
+    <p>
+      Move your mouse
+      <img src="<?php bloginfo('template_url'); ?>/images/homepage/mouse.png" /><br />
+      on each floor to see how it works
+    </p>
+    <a href="<?php print servula_info('full_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/homepage/start-shopping.png" /></a>
+    <br />
     (or
-    <a href="http://servula-wp/services">Learn More</a>)
+    <a href="<?php print site_url('/services'); ?>">Learn More</a>)
   </div>
   
   <div id="rightcolumn">
-    <img src="<?php bloginfo('template_url'); ?>/images/homepage-bee.png" />
+    <img src="<?php bloginfo('template_url'); ?>/images/homepage/bee.jpg" />
   </div>
   
   <div id="what-our-clients-say">
-    <h4>See what our clients say about Servula (<a href="<?php bloginfo('url'); ?>/testimonials">All Testimonials</a>)</h4>
+    <h4>
+      <img src="<?php bloginfo('template_url'); ?>/images/homepage/heart.png" />
+      See what our clients say about Servula (<a href="<?php print site_url('/testimonials'); ?>">All Testimonials</a>)
+    </h4>
     CLIENTS roll
   </div>
   
   <div id="all-services">
-    <h4>All Services in one place (<a href="<?php bloginfo('url'); ?>/services">All Services</a>)</h4>
+    <h4>All Services in one place (<a href="<?php print site_url('/services'); ?>">All Services</a>)</h4>
     SERVICES
   </div>
 </div>
+
 <?php get_footer(); ?>
