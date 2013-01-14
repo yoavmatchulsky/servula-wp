a4537c5f3677fb229427f872483670fd12c831ee
Yoav Matchulsky <yoavmatchulsky@gmail.com>
2013-01-14 10:33:25 +0200

    Added db-error page to theme

---
+++ db-error.php
@@ -0,0 +1,279 @@
+<?php
+	header('HTTP/1.1 503 Service Temporarily Unavailable');
+	header('Status: 503 Service Temporarily Unavailable');
+	header('Retry-After: 120');
+?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head profile="http://gmpg.org/xfn/11">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <link rel="alternate" type="application/rss+xml" title="Servula RSS Feed" href="http://www.servula.com/feed/" />
+  <link rel="pingback" href="http://www.servula.com/xmlrpc.php" />
+  <link rel="stylesheet" href="http://www.servula.com/wp-content/themes/servula/style.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="http://www.servula.com/wp-content/themes/servula/css/print.css" type="text/css" media="print" />
+  <!--[if IE]><link rel="stylesheet" href="http://www.servula.com/wp-content/themes/servula/css/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+    
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script src="http://www.servula.com/wp-content/themes/servula/jquery.fancybox.js" type="text/javascript"></script>
+
+  <script type="text/javascript" src="http://www.servula.com/wp-content/themes/servula/script.js"></script>
+  <script type="text/javascript">
+  /* <![CDATA[ */
+  var Servula = {
+    system_url : "https://my.servula.com",
+    func : {}
+  }
+  /* ]]> */
+
+  // ClickTale Top part
+  var WRInitTime=(new Date()).getTime();
+  </script>
+
+  <title>Servula - Error ? </title>
+  <link href="http://www.servula.com/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+  <script type="text/javascript">
+  
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24864094-1']);
+  _gaq.push(['_setDomainName', 'servula.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  
+  </script>
+  
+  <!--Start of Zopim Live Chat Script-->
+  <script type="text/javascript">
+  window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+  d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+  _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+  $.src='//cdn.zopim.com/?R1VnaN0Gk6rs1xQDY6HSVYndlysPOWIe';z.t=+new Date;$.
+  type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+  </script>
+  <!--End of Zopim Live Chat Script-->
+
+  <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.servula.com/wp-includes/wlwmanifest.xml" /> 
+  <meta name="generator" content="WordPress 3.4.1" />
+</head>
+<body>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+  
+	<header id="header">
+	  <div class="wrapper">
+	    <div id="logo-wrapper">
+	      <a href="http://www.servula.com">
+	        <img src="http://www.servula.com/wp-content/themes/servula/images/logo-beta.png" alt="Servula is currently in Beta" title="Servula is currently in Beta" />
+	      </a>
+	    </div>
+	    
+      <nav class="menu-main-container"><ul id="menu-main" class="menu"><li id="menu-item-106" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-106"><a href="/">Home</a></li>
+<li id="menu-item-109" class="menu-item menu-item-type-post_type menu-item-object-page page_item page-item-2 menu-item-109"><a href="http://www.servula.com/about/">About</a></li>
+<li id="menu-item-182" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-182"><a href="https://my.servula.com/">Services</a></li>
+<li id="menu-item-107" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-107"><a href="http://www.servula.com/blog/">Blog</a></li>
+<li id="menu-item-196" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-196"><a href="http://www.servula.com/contact/">Contact</a></li>
+</ul></nav>
+      <div class="info-wrapper">
+        <div class='info-item-wrapper'>
+          <img alt="Credits" height="24" src="http://www.servula.com/wp-content/themes/servula/images/header/gold-coin-24x24.png" width="24" />
+          <div class='info-item-text'>
+            <div class='uppercase'>
+              No Credits, Yet            </div>
+            <a href="https://my.servula.com/credits/plans">Buy Credits <span class="arrow-sign">&#9654;</span></a>
+          </div>
+        </div>
+        <div class='info-item-wrapper'>
+          <a href="https://my.servula.com/orders/checkout"><img alt="Go to Cart" height="24" src="http://www.servula.com/wp-content/themes/servula/images/header/cart-24x24.png" width="24" /></a>
+          <div class='info-item-text'>
+            <div class='uppercase'>Cart is Empty</div>
+            <a href="https://my.servula.com/">Order Services <span class="arrow-sign">&#9654;</span></a>
+          </div>
+        </div>
+      </div>
+      
+            <div class="login-register-wrapper">
+        <img src="http://www.servula.com/wp-content/themes/servula/images/header/down-arrow-16x16.png" width="16" height="16" />
+        <span class="login-register-button">Login/Register</span>
+        <div class="login-form-wrapper hidden">
+          <form action="https://my.servula.com/sessions" method="POST" accept-charset="UTF-8">
+            <input type="hidden" value="✓" name="utf8">
+            
+            <div class="form-item" style="margin-top: 0;">
+              <label for="session_email">Email:</label>
+              <input type="text" value="" tabindex="1" size="35" name="session[email]" id="session_email">
+            </div>
+            
+            <div class="form-item">
+              <span class="forgot-password"><a href="https://my.servula.com/password_resets/new">Forgot Password?</a></span>
+              <label for="session_password">Password:</label>
+              <input type="password" tabindex="2" size="35" name="session[password]" id="session_password">
+            </div>
+            
+            <div class="form-item">            
+              <input type="hidden" value="0" name="session[remember_me]"><input type="checkbox" value="1" name="session[remember_me]" id="session_remember_me">
+              <label for="session_remember_me">Remember Me</label>
+            </div>
+            
+            <input type="submit" value="Login" tabindex="3" name="commit">
+            Or
+            <a href="https://my.servula.com/register">Register now</a>
+          </form>
+        </div>
+      </div>
+            
+    </div>
+    
+  </header>
+  <div class="wrapper clearfix">
+
+<div id="leftcolumn">
+  <ul id="sidebar">
+
+<li id="nav_menu-3" class="widget widget_nav_menu"><div class="menu-inner-page-sidebar-container"><ul id="menu-inner-page-sidebar" class="menu"><li id="menu-item-127" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-127"><a href="http://www.servula.com/about/">How it works?</a></li>
+<li id="menu-item-135" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-135"><a href="http://www.servula.com/faq/">FAQ</a></li>
+<li id="menu-item-208" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-208"><a href="http://www.servula.com/contact/">Contact</a></li>
+<li id="menu-item-134" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-134"><a href="http://www.servula.com/jobs/">Jobs</a></li>
+<li id="menu-item-133" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-133"><a href="/blog/">Blog</a></li>
+</ul></div></li><li id="text-3" class="widget widget_text">			<div class="textwidget"><div class="fb-like-box" data-href="https://www.facebook.com/pages/Servula/181501315237979" data-width="182" data-show-faces="true" data-stream="false" data-header="false"></div></div>
+		</li></ul>
+
+</div>
+
+<div id="rightcolumn">
+  
+	  
+	  <div class="post" id="post-2">
+      <h1 style="margin-bottom: 10px;">Oh, man!</h1>
+      
+      <h3>Yeah, we hate error pages, too...</h3>
+
+      <p>Someone has probably been messing with the system. Don't worry, though. We'll go yell at them to fix the problem ASAP!</p>
+
+      <p>The page should load again in a few minutes.</p>
+
+      <p>To pass the time, you can play <strong>Space Invaders</strong>:</p>
+      
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="400" HEIGHT="500">
+        <param name="quality" value="high">
+        <param name="src" value="http://www.coolretrogames.com/invaders.swf">
+        <embed allowScriptAccess="never" allowNetworking="internal" enableJavaScript="false" src="http://www.coolretrogames.com/invaders.swf" width="500" height="400" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash"></embed>
+      </object>
+      
+		  <div class="postdata"></div>
+	  </div>
+
+  </div>
+</div>
+<footer>
+  <div class="wrapper">
+    <ul>
+      <li>
+        <h3>Learn About Us</h3>
+        <ul>
+          <li><a href="http://www.servula.com/about/">What we do?</a></li>
+          <li><a href="http://www.servula.com/faq/">FAQ</a></li>
+          <li><a href="http://www.servula.com/jobs/">Jobs</a></li>
+        </ul>
+      </li>
+      <li>
+        <ul>
+          <h3>&nbsp;</h3>
+          <li><a href="http://www.servula.com/contact/">Contact Us</a></li>
+          <li><a href="http://www.servula.com/blog/">Blog</a></li>
+        </ul>
+      </li>
+      <li>
+        <h3>General</h3>
+        <ul>
+          <li><a href="https://my.servula.com">Our Services</a></li>
+          <li><a href="https://my.servula.com/register">Register Now!</a></li>
+          <li><a href="https://my.servula.com/terms-of-service">Terms of Service</a></li>
+        </ul>
+      </li>
+      <li>
+        <h3>Spread the word!</h3>
+        <ul class="social-links">
+                    <li><a href="https://twitter.com/servulashop" class="twitter-follow-button" data-show-screen-name="false" data-show-count="false" data-lang="en" data-width="90px">Follow @servulashop</a></li>
+          <li><iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FServula%2F181501315237979&locale=en_US&amp;layout=button_count&amp;show_faces=false&amp;width=90&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:90px; height:21px;" allowTransparency="true"></iframe></li>
+          <li><g:plusone size="medium" href="http://www.servula.com/"></g:plusone></li>
+        </ul>
+        
+        <ul class="social-links social-links-images">
+          <li><a id="social-links-image-linked-in" href="#"></a></li>
+          <li><a id="social-links-image-twitter" href="https://twitter.com/#!/ServulaShop"></a></li>
+          <li><a id="social-links-image-facebook" href="http://www.facebook.com/pages/Servula/181501315237979"></a></li>
+        </ul>
+      </li>
+    </ul>
+    
+    <div class="newsletter-wrapper">
+      <img src="http://www.servula.com/wp-content/themes/servula/images/footer/servula-logo.png" />
+      
+      <div class="newsletter-bubble">
+        <div class="newsletter-text">
+          <strong>Hey! Get over here.</strong>
+          Sign up for Servula's newsletter and get updates about our latest activities,
+          blog posts, special offers and cool tips!
+        </div>
+        
+        <form action="http://app.icontact.com/icp/signup.php" name="icpsignup" id="icpsignup5242" method="post" accept-charset="UTF-8">
+          <input type="hidden" value="http://www.icontact.com/www/signup/thanks.html" name="redirect">
+          <input type="hidden" value="http://www.icontact.com/www/signup/error.html" name="errorredirect">        
+          <input type="hidden" value="56024" name="listid">
+          <input type="hidden" value="LUVM" name="specialid:56024">
+          <input type="hidden" value="1063486" name="clientid">
+          <input type="hidden" value="5242" name="formid">
+          <input type="hidden" value="1" name="reallistid">
+          <input type="hidden" value="0" name="doubleopt">          
+
+          <input type="submit" value="Sign Up" tabindex="102" />
+          <input type="text" name="fields_fname" value="" placeholder="Name" tabindex="100" />
+          <input type="text" name="fields_email" value="" placeholder="Email" tabindex="101" />
+        </form>
+      </div>
+    </div>
+    <div class="copyright-wrapper">
+  	  &copy; 2012 <a href="http://www.servula.com">Servula</a>, LLC. All right reserved.
+  	</div>
+  </div>
+</footer>
+
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+<!-- ClickTale Bottom part -->
+<div id="ClickTaleDiv" style="display: none;"></div>
+<script type="text/javascript">
+if(document.location.protocol!='https:')
+  document.write(unescape("%3Cscript%20src='http://s.clicktale.net/WRc9.js'%20type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+if(typeof ClickTale=='function') ClickTale(23332,1,"www02");
+</script>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<script type='text/javascript' src='http://www.servula.com/wp-content/plugins/contact-form-7/jquery.form.js?ver=3.08'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpcf7 = {"loaderUrl":"http:\/\/www.servula.com\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.servula.com/wp-content/plugins/contact-form-7/scripts.js?ver=3.1.2'></script>
+
+</body>
+</html>
