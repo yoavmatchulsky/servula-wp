c879ce0e5fffb1c8030f3747aaea62a28b47b149
Yoav Matchulsky <yoavmatchulsky@gmail.com>
2013-04-27 15:58:18 +0300

    Fixed index page's layout

--- index.php
+++ index.php
@@ -1,41 +1,46 @@
-<?php get_header(); ?>
+<?php
 
-<div id="content-wrapper">
+global $servula;
+$servula['body_class'] = 'index';
 
+?><?php get_header(); ?>
+
+<div id="leftcolumn">
   <?php get_sidebar(); ?>
+</div>
 
-  <div id="rightcolumn">
+<div id="rightcolumn">
 
-  <?php if (have_posts()) : ?>
+<?php if (have_posts()) : ?>
 
-	  <?php while (have_posts()) : the_post(); ?>
-    <article>
-	    <div class="post" id="post-<?php the_ID(); ?>">
-		    <h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+  <?php while (have_posts()) : the_post(); ?>
+  <article>
+    <div class="post" id="post-<?php the_ID(); ?>">
+	    <h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-		    <div class="entry">
-			    <?php the_content('Read More...'); ?>
-		    </div>
+	    <div class="entry">
+		    <?php the_content('Read More...'); ?>
 	    </div>
-    </article>
+    </div>
+  </article>
 
-	  <?php endwhile; ?>
+  <?php endwhile; ?>
 
-      <div class="pagers clearfix">
-          <p class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></p>
-          <p class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></p>
-      </div>
+    <div class="pagers clearfix">
+        <p class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></p>
+        <p class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></p>
+    </div>
 
-  <?php else : ?>
+<?php else : ?>
 
-    <article>
-	    <div class="post">
-		    <div class="title"><h2>Not Found</h2></div>
-		    <p>Sorry, but you are looking for something that isn't here.</p>
-	    </div>
-    </article>
+  <article>
+    <div class="post">
+	    <div class="title"><h2>Not Found</h2></div>
+	    <p>Sorry, but you are looking for something that isn't here.</p>
+    </div>
+  </article>
 
-  <?php endif; ?>
-  </div>
-</div>       
+<?php endif; ?>
+</div>
+
 <?php get_footer(); ?>
