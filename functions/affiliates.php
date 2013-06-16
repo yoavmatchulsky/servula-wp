fe1d43e82919cf02255469e9013a04e9a0ea5ee1
Yoav Matchulsky <yoavmatchulsky@gmail.com>
2013-06-16 12:07:25 +0300

    Don't set an empty referer in affiliates

--- functions/affiliates.php
+++ functions/affiliates.php
@@ -7,14 +7,17 @@ function servula_affiliates() {
   if (isset($_REQUEST[SERVULA_AFFILIATES_QUERY_PARAMETER])) {
     $aff_id = intval($_REQUEST[SERVULA_AFFILIATES_QUERY_PARAMETER]);
     if ($aff_id > 0) {
+      $referer    = $_SERVER['HTTP_REFERER'];
       $time       = time();
       $next_month = $time + 2592000;
       $domain     = servula_info('cookie_domain');
       $path       = '/';
       
       setcookie('aff_tag', $aff_id, $next_month, $path, $domain);
-      setcookie('aff_from', $_SERVER['HTTP_REFERER'], $next_month, $path, $domain);
       setcookie('aff_time', $time, $next_month, $path, $domain);
+      if (!empty($referer)) {
+        setcookie('aff_from', $referer, $next_month, $path, $domain);
+      }
     }
   }
 }
