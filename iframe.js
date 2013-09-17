dc0f2c9e21afbb4f31d073d3069c6e14b2ed71da
Yoav Matchulsky <yoavmatchulsky@gmail.com>
2013-09-17 11:05:25 +0200

    Refine error messages for lack of proper parent

--- iframe.js
+++ iframe.js
@@ -1,24 +1,24 @@
 jQuery(function() {
+  var _error;
   try {
     var parent = window.parent;
-    if (typeof Servula !== "undefined" && Servula !== null && (parent.servula_info != null)) {
+    if (typeof Servula !== "undefined" && Servula !== null && parent.servula_info != null) {
       Servula.iframe = parent.servula_info;
-    }
 
-    if ((Servula.iframe.trigger_resize != null) && (typeof Servula.iframe.trigger_resize === 'function')) {
-      $('body').on('servula:iframe:resize', function() {
-        Servula.iframe.trigger_resize();
-      });
-    }
+      if (Servula.iframe.trigger_resize != null && typeof Servula.iframe.trigger_resize === 'function') {
+        $('body').on('servula:iframe:resize', function() {
+          Servula.iframe.trigger_resize();
+        });
+      }
 
-    if ((Servula.iframe.trigger_loaded != null) && (typeof Servula.iframe.trigger_loaded === 'function')) {
-      $('body').on('servula:iframe:loaded', function() {
-        Servula.iframe.trigger_loaded();
-      });
+      if (Servula.iframe.trigger_loaded != null && typeof Servula.iframe.trigger_loaded === 'function') {
+        $('body').on('servula:iframe:loaded', function() {
+          Servula.iframe.trigger_loaded();
+        });
+      }
     }
-
   } catch (_error) {
-    console && console.log('Error? ', _error);
+    console && console.warn("I can't access my parent!", _error);
   } finally {
     if (Servula.iframe == null) {
       Servula.iframe = {};
